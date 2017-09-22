1ca9553c06c83f58581125c961e45d282cf502f9
Byeonggon Lee <gonny952@gmail.com>
2017-09-22 20:11:54 +0900

    test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,9 @@
 local hangul = require 'hangul'
 local hangul_ic = hangul.ic_new()
-print(hangul_ic:process(string.byte('a')))
-print(hangul_ic:process(string.byte('k')))
-print(hangul_ic:process(string.byte('s')))
-local result = hangul_ic:flush()
-print(hangul.ucs4_to_utf8(result))
-
+while true do
+    local char = string.byte(io.read(1))
+    if char ~= '\n' then
+        hangul_ic:process(char)
+    end
+    print(hangul.ucs4_to_utf8(hangul_ic:get_preedit_string()))
+end
