7a39cecb305d013e328662213ab2934d7931cbf5
Byeonggon Lee <gonny952@gmail.com>
2019-07-26 14:04:02 +0900

    Fix test code

--- test/test.lua
+++ test/test.lua
@@ -1,15 +1,8 @@
-package.cpath = '../build/src/?.so;' .. package.cpath
+package.cpath = '../?.so;' .. package.cpath
 local hangul = require 'hangul'
 local hangul_ic = hangul.ic_new()
-local text = ''
 while true do
     local char = string.byte(io.read(1))
-    if char ~= '\n' then
-        hangul_ic:process(char)
-    end
-    local commitString = hangul.ucs4_to_utf8(hangul_ic:get_commit_string())
-    if commitString ~= '' then
-        text = text .. commitString
-        print(text)
-    end
+    hangul_ic:process(char)
+    io.write(hangul.ucs4_to_utf8(hangul_ic:get_commit_string()))
 end
